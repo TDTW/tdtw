2a8cd93d8e11e051c34de6b0c4bfb39c96ddde78
PsychoGod <temhbiumulllka@gmail.com>
2013-11-08 17:36:38 +0200

    [0.6.2.82] - new feature without localization, just english and russian

--- src/game/client/components/test.cpp
+++ src/game/client/components/test.cpp
@@ -3,6 +3,7 @@
 #include <engine/demo.h>
 #include <engine/graphics.h>
 #include <engine/textrender.h>
+#include <engine/shared/config.h>
 #include <game/generated/protocol.h>
 #include <game/generated/client_data.h>
 
@@ -48,110 +49,258 @@ void CTest::Create(vec2 Pos, int Type)
 	CItem *i = CreateI();
 	if (i)
 	{
-		i->m_StartPos = vec2(Pos.x + rand()%80-40, Pos.y - 50);
-		i->m_EndPos = vec2(Pos.x + rand()%80-40, Pos.y - 70);
+		i->m_StartPos = vec2(Pos.x, Pos.y - 50);
+		i->m_EndPos = vec2(Pos.x + rand()%80-40, Pos.y - 80);
 		i->m_StartTime = Client()->LocalTime();
 		i->m_Speed = S_FAST;
 		i->m_Size = Z_SMAL;
 		
-		switch(Type)
+		if(str_find(g_Config.m_ClLanguagefile, "russian") || 
+			str_find(g_Config.m_ClLanguagefile, "ukrainian") ||
+			str_find(g_Config.m_ClLanguagefile, "belarusian"))
 		{
-		case SOUND_GUN_FIRE: // тра - та та та та 
-			if(SearchType(SOUND_GUN_FIRE))
-				str_copy(i->m_Text, Localize("Та"), sizeof(i->m_Text));
-			else			
-				str_copy(i->m_Text, Localize("Тра"), sizeof(i->m_Text));
-			break;
-		case SOUND_SHOTGUN_FIRE: // пух - пыщ
-			if(rand()%2==0)
-				str_copy(i->m_Text, Localize("Пух"), sizeof(i->m_Text));
-			else
-				str_copy(i->m_Text, Localize("Пыщ"), sizeof(i->m_Text));
-			break;
-		case SOUND_GRENADE_FIRE: // Бум
-			str_copy(i->m_Text, Localize("Бум"), sizeof(i->m_Text));
-			break;
-		case SOUND_GRENADE_EXPLODE: // бдыщ
-			str_copy(i->m_Text, Localize("БДЫЩ"), sizeof(i->m_Text));
-			i->m_Speed = S_SLOW;
-			i->m_Size = Z_LARG;
-			break;
-		case SOUND_HAMMER_FIRE: // фух
-			str_copy(i->m_Text, Localize("Фух"), sizeof(i->m_Text));
-			break;
-		case SOUND_HAMMER_HIT: // дыщ
-			str_copy(i->m_Text, Localize("Дыщ"), sizeof(i->m_Text));
-			i->m_Size = Z_MID;
-			break;
-		case SOUND_NINJA_FIRE: // кьяяя
-			str_copy(i->m_Text, Localize("КЬЯЯ"), sizeof(i->m_Text));
-			i->m_Size = Z_MID;
-			break;
-		case SOUND_PICKUP_NINJA: // кьяяя
-			str_copy(i->m_Text, Localize("КЬЯЯ"), sizeof(i->m_Text));
-			i->m_Speed = S_SLOW;
-			i->m_Size = Z_LARG;
-			break;
-		case SOUND_NINJA_HIT: // хряк
-			str_copy(i->m_Text, Localize("Хряк"), sizeof(i->m_Text));
-			i->m_Size = Z_LARG;
-			break;
-		case SOUND_RIFLE_FIRE: // дзз
-		case SOUND_RIFLE_BOUNCE: // дзз
-			str_copy(i->m_Text, Localize("Дзз"), sizeof(i->m_Text));
-			break;
-		case SOUND_WEAPON_SWITCH: // фух
-			str_copy(i->m_Text, Localize("Фух"), sizeof(i->m_Text));
-			break;
-		case SOUND_PLAYER_PAIN_SHORT: // Ааа
-			str_copy(i->m_Text, Localize("Ааа"), sizeof(i->m_Text));
-			i->m_Speed = S_MID;
-			i->m_Size = Z_MID;
-			break;
-		case SOUND_PLAYER_PAIN_LONG: // АаАаА
-			str_copy(i->m_Text, Localize("АаАаа"), sizeof(i->m_Text));
-			i->m_Speed = S_SLOW;
-			i->m_Size = Z_LARG;
-			break;
-		case SOUND_PLAYER_DIE: // Труп
-			str_copy(i->m_Text, Localize("Труп"), sizeof(i->m_Text));
-			i->m_Speed = S_MID;
-			i->m_Size = Z_LARG;
-			break;
-		case SOUND_PLAYER_SPAWN: // Вжух
-			str_copy(i->m_Text, Localize("Вжух"), sizeof(i->m_Text));
-			i->m_Speed = S_MID;
-			i->m_Size = Z_LARG;
-			break;
-		case SOUND_PLAYER_SKID: // УИИИ
-			str_copy(i->m_Text, Localize("УИИИ"), sizeof(i->m_Text));
-			i->m_Speed = S_MID;
-			i->m_Size = Z_MID;
-			break;
+			switch(Type)
+			{
+			case SOUND_GUN_FIRE: // тра - та та та та 
+				if(SearchType(SOUND_GUN_FIRE))
+				{
+					if(rand()%2==0)
+						str_copy(i->m_Text, "Та", sizeof(i->m_Text)); //Та
+					else
+						str_copy(i->m_Text, "Да", sizeof(i->m_Text));	//Да			
+				}
+				else
+				{			
+					str_copy(i->m_Text, "Тра", sizeof(i->m_Text)); //Тра
+					i->m_Size = Z_LARG;
+				}
+				break;
+			case SOUND_SHOTGUN_FIRE: // пух - пыщ
+				if(rand()%2==0)
+					str_copy(i->m_Text, "Пух", sizeof(i->m_Text));
+				else
+					str_copy(i->m_Text, "Пыщ", sizeof(i->m_Text));
+				break;
+			case SOUND_GRENADE_FIRE: // Бум
+				str_copy(i->m_Text, "Бум", sizeof(i->m_Text));
+				break;
+			case SOUND_GRENADE_EXPLODE: // бдыщ
+				str_copy(i->m_Text, "БДЫЩ", sizeof(i->m_Text));
+				i->m_Speed = S_SLOW;
+				i->m_Size = Z_LARG;
+				break;
+			case SOUND_HAMMER_FIRE: // фух
+				str_copy(i->m_Text, "Фух", sizeof(i->m_Text));
+				break;
+			case SOUND_HAMMER_HIT: // дыщ
+				str_copy(i->m_Text, "Дыщ", sizeof(i->m_Text));
+				i->m_Size = Z_MID;
+				break;
+			case SOUND_NINJA_FIRE: // кьяяя
+				str_copy(i->m_Text, "КЬЯЯ", sizeof(i->m_Text));
+				i->m_Size = Z_MID;
+				break;
+			case SOUND_PICKUP_NINJA: // кьяяя
+				str_copy(i->m_Text, "КЬЯЯ", sizeof(i->m_Text));
+				i->m_Speed = S_SLOW;
+				i->m_Size = Z_LARG;
+				break;
+			case SOUND_NINJA_HIT: // хряк
+				str_copy(i->m_Text, "Хряк", sizeof(i->m_Text));
+				i->m_Size = Z_LARG;
+				break;
+			case SOUND_RIFLE_FIRE: // дзз
+				str_copy(i->m_Text, "Пиу", sizeof(i->m_Text));
+				break;
+			case SOUND_RIFLE_BOUNCE: // дзз
+				str_copy(i->m_Text, "Дзз", sizeof(i->m_Text));
+				break;
+			case SOUND_WEAPON_SWITCH: // фух
+				str_copy(i->m_Text, "Фух", sizeof(i->m_Text));
+				break;
+			case SOUND_PLAYER_PAIN_SHORT: // Ааа
+				if(rand()%3==0)
+					str_copy(i->m_Text, "Ааа", sizeof(i->m_Text));
+				else if(rand()%3==1)
+					str_copy(i->m_Text, "Аай", sizeof(i->m_Text));
+				else
+					str_copy(i->m_Text, "Оой", sizeof(i->m_Text));
+				i->m_Speed = S_MID;
+				i->m_Size = Z_MID;
+				break;
+			case SOUND_PLAYER_PAIN_LONG: // АаАаА
+				str_copy(i->m_Text, "АаАаа", sizeof(i->m_Text));
+				i->m_Speed = S_SLOW;
+				i->m_Size = Z_LARG;
+				break;
+			case SOUND_PLAYER_DIE: // Труп
+				if(rand()%3==0)
+					str_copy(i->m_Text, "Труп", sizeof(i->m_Text));
+				else if(rand()%3==1)
+					str_copy(i->m_Text, "Здох", sizeof(i->m_Text));
+				else
+					str_copy(i->m_Text, "Умер", sizeof(i->m_Text));
+				i->m_Speed = S_SLOW;
+				i->m_Size = Z_LARG;
+				break;
+			case SOUND_PLAYER_SPAWN: // Вжух
+				str_copy(i->m_Text, "Вжух", sizeof(i->m_Text));
+				i->m_Speed = S_MID;
+				i->m_Size = Z_LARG;
+				break;
+			case SOUND_PLAYER_SKID: // УИИИ
+				str_copy(i->m_Text, "УИИии", sizeof(i->m_Text));
+				i->m_Speed = S_MID;
+				i->m_Size = Z_MID;
+				break;
 
-		case SOUND_PICKUP_HEALTH: // Уоп
-			str_copy(i->m_Text, Localize("Уоп"), sizeof(i->m_Text));
-			i->m_Speed = S_MID;
-			i->m_Size = Z_MID;
-			break;
-		case SOUND_WEAPON_NOAMMO: // Чик
-			str_copy(i->m_Text, Localize("Чик"), sizeof(i->m_Text));
-			break;
-		case SOUND_PICKUP_ARMOR: // Чик
-		case SOUND_PICKUP_GRENADE: // Чик
-		case SOUND_PICKUP_SHOTGUN: // Чик
-			str_copy(i->m_Text, Localize("Чик"), sizeof(i->m_Text));
-			i->m_Speed = S_MID;
-			i->m_Size = Z_MID;
-			break;
-		case SOUND_WEAPON_SPAWN: // Уоп
-			str_copy(i->m_Text, Localize("Уоп"), sizeof(i->m_Text));
-			i->m_Speed = S_SLOW;
-			i->m_Size = Z_MID;
-			break;
-		default:
-			DestroyI(i);
-			return;
+			case SOUND_PICKUP_HEALTH: // Уоп
+				str_copy(i->m_Text, "Уоп", sizeof(i->m_Text));
+				i->m_Speed = S_MID;
+				i->m_Size = Z_MID;
+				break;
+			case SOUND_WEAPON_NOAMMO: // Чик
+				str_copy(i->m_Text, "Чик", sizeof(i->m_Text));
+				break;
+			case SOUND_PICKUP_ARMOR: // Чик
+				str_copy(i->m_Text, "Чик", sizeof(i->m_Text));
+				i->m_Speed = S_MID;
+				i->m_Size = Z_MID;
+				break;
+			case SOUND_PICKUP_GRENADE: // Чик
+			case SOUND_PICKUP_SHOTGUN: // Чик
+				str_copy(i->m_Text, "Пик", sizeof(i->m_Text));
+				i->m_Speed = S_MID;
+				i->m_Size = Z_MID;
+				break;
+			case SOUND_WEAPON_SPAWN: // Уоп
+				str_copy(i->m_Text, "Уоп", sizeof(i->m_Text));
+				i->m_Speed = S_SLOW;
+				i->m_Size = Z_MID;
+				break;
+			default:
+				DestroyI(i);
+				return;
+			}
+		}
+		else
+		{
+			switch(Type)
+			{
+			case SOUND_GUN_FIRE: // тра - та та та та 
+				if(SearchType(SOUND_GUN_FIRE))
+				{
+					str_copy(i->m_Text, "Ta", sizeof(i->m_Text));				
+				}
+				else
+				{			
+					str_copy(i->m_Text, "Ra", sizeof(i->m_Text)); 
+					i->m_Size = Z_LARG;
+				}
+				break;
+			case SOUND_SHOTGUN_FIRE: // пух - пыщ
+				if(rand()%2==0)
+					str_copy(i->m_Text, "Bang", sizeof(i->m_Text));
+				else
+					str_copy(i->m_Text, "Blam", sizeof(i->m_Text));
+				break;
+			case SOUND_GRENADE_FIRE: // Бум
+				str_copy(i->m_Text, "Poom", sizeof(i->m_Text));
+				break;
+			case SOUND_GRENADE_EXPLODE: // бдыщ
+				str_copy(i->m_Text, "BOOM", sizeof(i->m_Text));
+				i->m_Speed = S_SLOW;
+				i->m_Size = Z_LARG;
+				break;
+			case SOUND_HAMMER_FIRE: // фух
+				str_copy(i->m_Text, "Hop", sizeof(i->m_Text));
+				break;
+			case SOUND_HAMMER_HIT: // дыщ
+				str_copy(i->m_Text, "Hit", sizeof(i->m_Text));
+				i->m_Size = Z_MID;
+				break;
+			case SOUND_NINJA_FIRE: // кьяяя
+				str_copy(i->m_Text, "KYaaaw", sizeof(i->m_Text));
+				i->m_Size = Z_MID;
+				break;
+			case SOUND_PICKUP_NINJA: // кьяяя
+				str_copy(i->m_Text, "KYaaaw", sizeof(i->m_Text));
+				i->m_Speed = S_SLOW;
+				i->m_Size = Z_LARG;
+				break;
+			case SOUND_NINJA_HIT: // хряк
+				str_copy(i->m_Text, "SLash", sizeof(i->m_Text));
+				i->m_Size = Z_LARG;
+				break;
+			case SOUND_RIFLE_FIRE: // дзз
+				str_copy(i->m_Text, "Phew", sizeof(i->m_Text));
+				break;
+			case SOUND_RIFLE_BOUNCE: // дзз
+				str_copy(i->m_Text, "Zap", sizeof(i->m_Text));
+				break;
+			case SOUND_WEAPON_SWITCH: // фух
+				str_copy(i->m_Text, "Wosh", sizeof(i->m_Text));
+				break;
+			case SOUND_PLAYER_PAIN_SHORT: // Ааа
+				if(rand()%2==0)
+					str_copy(i->m_Text, "Aaa", sizeof(i->m_Text));
+				else
+					str_copy(i->m_Text, "Ouch", sizeof(i->m_Text));			
+				i->m_Speed = S_MID;
+				i->m_Size = Z_MID;
+				break;
+			case SOUND_PLAYER_PAIN_LONG: // АаАаА
+				str_copy(i->m_Text, "AaAaa", sizeof(i->m_Text));
+				i->m_Speed = S_SLOW;
+				i->m_Size = Z_LARG;
+				break;
+			case SOUND_PLAYER_DIE: // Труп
+				if(rand()%2==0)
+					str_copy(i->m_Text, "Died", sizeof(i->m_Text));
+				else
+					str_copy(i->m_Text, "Crack", sizeof(i->m_Text));
+				i->m_Speed = S_SLOW;
+				i->m_Size = Z_LARG;
+				break;
+			case SOUND_PLAYER_SPAWN: // Вжух
+				str_copy(i->m_Text, "Yey!", sizeof(i->m_Text));
+				i->m_Speed = S_MID;
+				i->m_Size = Z_LARG;
+				break;
+			case SOUND_PLAYER_SKID: // УИИИ
+				str_copy(i->m_Text, "Shh", sizeof(i->m_Text));
+				i->m_Speed = S_MID;
+				i->m_Size = Z_MID;
+				break;
+			case SOUND_PICKUP_HEALTH: // Уоп
+				str_copy(i->m_Text, "Whop", sizeof(i->m_Text));
+				i->m_Speed = S_MID;
+				i->m_Size = Z_MID;
+				break;
+			case SOUND_WEAPON_NOAMMO: // Чик
+				str_copy(i->m_Text, "Click", sizeof(i->m_Text));
+				break;
+			case SOUND_PICKUP_ARMOR: // Чик
+				str_copy(i->m_Text, "Trick", sizeof(i->m_Text));
+				i->m_Speed = S_MID;
+				i->m_Size = Z_MID;
+				break;
+			case SOUND_PICKUP_GRENADE: // Чик
+			case SOUND_PICKUP_SHOTGUN: // Чик
+				str_copy(i->m_Text, "Pick", sizeof(i->m_Text));
+				i->m_Speed = S_MID;
+				i->m_Size = Z_MID;
+				break;
+			case SOUND_WEAPON_SPAWN: // Уоп
+				str_copy(i->m_Text, "Whop", sizeof(i->m_Text));
+				i->m_Speed = S_SLOW;
+				
+				i->m_Size = Z_MID;
+				break;
+			default:
+				DestroyI(i);
+				return;
+			}
 		}
 		
 		i->m_Type = Type;
@@ -224,6 +373,10 @@ void CTest::OnRender()
 			i++;
 			if(m_aItems[i].m_StartPos.y > m_aItems[i].m_EndPos.y)
 				m_aItems[i].m_StartPos.y--;
+			if(m_aItems[i].m_StartPos.x > m_aItems[i].m_EndPos.x)
+				m_aItems[i].m_StartPos.x--;
+			if(m_aItems[i].m_StartPos.x < m_aItems[i].m_EndPos.x)
+				m_aItems[i].m_StartPos.x++;
 		}
 	}	
 	s_LastLocalTime = Client()->LocalTime();
